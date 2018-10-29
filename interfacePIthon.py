2f9d8e33416804e1369c9dbb9c99682e06e3bea1
unknown <michael.hopwood@knights.ucf.edu>
2018-10-29 11:32:50 -0400

    Added logging, updated and split functions

--- interfacePIthon.py
+++ interfacePIthon.py
@@ -25,29 +25,14 @@ from OSIsoft.AF.UnitsOfMeasure import *
 # PI Data Archive
 piServers = PIServers()
 piServer = piServers['net1552.net.ucf.edu']
-
-def Pull_PI_Data(pitag, start, end, freq, timestampcalc = AFTimestampCalculation.MostRecentTime, summarytype = AFSummaryTypes.Maximum):
-    '''Creates dataframe of historical max hourly values for a single PI point'''
+    
+def get_tag_snapshot(tagname):  
     piServers = PIServers()
     piServer = piServers['net1552.net.ucf.edu']
-    pt = PIPoint.FindPIPoint(piServer, pitag)
-    timerange = AFTimeRange(start,end)
-    span = AFTimeSpan.Parse(freq)
-    summaries = pt.Summaries(timerange, span, summarytype, AFCalculationBasis.TimeWeighted, timestampcalc)
-    # Loop through and make list
-    times = []
-    vals = []
-    for summary in summaries:
-        for event in summary.Value:
-            times.append(str(event.Timestamp.LocalTime))
-            if type(event.Value) is PIException:
-                vals.append(None)
-            else:
-                vals.append(event.Value)
-    # Create dataframe
-    df = pd.DataFrame(data = {pitag: vals}, index=times)
-    df.index = pd.to_datetime(df.index)
-    
+    tag = PIPoint.FindPIPoint(piServer, tagname)  
+    lastData = tag.Snapshot()  
+    return lastData.Value
+
 def Store_Vals(df, valuecol, pointname):
     '''
     Store values into the PI System
@@ -64,12 +49,12 @@ def Store_Vals(df, valuecol, pointname):
     writeptname = writept.Name.lower()
     
     for row in df.itertuples():
-        time.sleep(0.010)
+        time.sleep(0.0005)
         val = AFValue()
         val.Value = float(row.vals)
         timed = AFTime(str(row.Index))
         val.Timestamp = timed  
-        writept.UpdateValue(val, AFUpdateOption.Replace, AFBufferOption.Buffer)
+        writept.UpdateValue(val, AFUpdateOption.Replace, AFBufferOption.BufferIfPossible)
         
 def run_mysql(query, host, port, database, username, password):
     '''Connect to the MySQL server with specified information'''
@@ -89,7 +74,7 @@ def run_mysql(query, host, port, database, username, password):
     mydb.close()
     return data 
 
-def reformat_IV(trace_id, parameter, string):
+def get_mysql_data(trace_id, parameter, string):
     '''
     Parameters:
     trace_id: specify the trace_id that is used    
@@ -98,42 +83,52 @@ def reformat_IV(trace_id, parameter, string):
     '''
 
     # query the trace_id in mySQL
-    trace_id_query = "SELECT CAST(datetime AS datetime), CAST(groupchannel_name as BINARY), trace_id FROM iv.trace ORDER BY datetime DESC;"
+    trace_id_query = "SELECT CAST(datetime AS datetime), CAST(groupchannel_name as BINARY), trace_id FROM iv.trace WHERE groupchannel_name = " +f'"{string}"'  + "ORDER BY datetime DESC;"
     start1_time =time.time()
     traceid_list = run_mysql(trace_id_query, host, port, database, username, password)
     f.write("Query trace ID time\n--- %s seconds ---\n" % (time.time() - start1_time))
+    f.flush()
     
+    # Create dataframe of datetime, groupchannel_name, and trace_id
+    df_trace = pd.DataFrame(traceid_list, columns=['datetime', 'group', 'trace_id']) 
     
     # Query the current trace_id in the PI System
-    S1_PI_traceid_df = Pull_PI_Data(trace_id, '*-1m', '*', '1m')
-
-    # If no value present in PI System, set trace_id locally to 1
+    cur_traceid = int(get_tag_snapshot(trace_id))
+    
+    # If no value present in PI System, set trace_id locally to 1100
     # else, use the last value in PI System
-    if S1_PI_traceid_df is None:
-        S1_PI_traceid = 1
+    if cur_traceid is None:
+        if string == '8157_S1':
+            cur_traceid = 1141
+        elif string == '8157_S2':
+            cur_traceid = 1140
+    elif cur_traceid == df_trace['trace_id'][0]:
+        f.write("No updated values.\nSleeping until next prompt...\n")
+        f.flush()
+        sys.exit()
     else:
-        S1_PI_traceid = S1_PI_traceid_df[S1_PI_traceid_df.columns[0]][0]
-        
-    # Create dataframe of datetime, groupchannel_name, and trace_id
-    df_trace_S1 = pd.DataFrame(traceid_list, columns=['datetime', 'group', 'trace_id'])
+        cur_traceid = int(get_tag_snapshot(trace_id))
 
+        
+    f.write("Using trace_id: {0}\n".format(cur_traceid))
+    f.flush()
 
     # Get index of sql table where the last pi value is located  
-    index_PiVal_S1 = df_trace_S1[df_trace_S1['trace_id'] == S1_PI_traceid].index.tolist()
+    index_PiVal = df_trace[df_trace['trace_id'] == cur_traceid].index.tolist()
     
     # String value of latest index in PI System
-    index_query_S1 = str(index_PiVal_S1[0])
+    index_query = str(index_PiVal[0])
     
     # concatenate SQL query to have the index_query length 
-    S1query = "SELECT CAST(datetime AS datetime), CAST(groupchannel_name as BINARY), trace_id, exclude, " + parameter + " FROM iv.trace WHERE groupchannel_name = " +f'"{string}"'  + " ORDER BY datetime DESC LIMIT 0, " + index_query_S1 + ";"
-    print("here")
+    query = "SELECT CAST(datetime AS datetime), CAST(groupchannel_name as BINARY), trace_id, exclude, " + parameter + " FROM iv.trace WHERE groupchannel_name = " +f'"{string}"'  + " ORDER BY datetime DESC LIMIT 0, " + index_query + ";"
+
     # Run this query
     start_time = time.time()
-    S1_temp_list = run_mysql(S1query, host, port, database, username, password)
+    temp_list = run_mysql(query, host, port, database, username, password)
     f.write("Query render time\n--- %s seconds ---\n" % (time.time() - start_time))
-
+    f.flush()
     #Create dataframe of parameters in query
-    df = pd.DataFrame(S1_temp_list, columns=['datetime', 'group', 'trace_id', 'exclude', parameter])
+    df = pd.DataFrame(temp_list, columns=['datetime', 'group', 'trace_id', 'exclude', parameter])
 
     # drop rows where exclude = 1
     df["exclude"] = df["exclude"].apply(lambda x: np.NaN if x == 1 else x)
@@ -141,7 +136,11 @@ def reformat_IV(trace_id, parameter, string):
     
     # Ensure that dataframe is in order
     df.sort_values(['trace_id'], inplace=True)
+    df.set_index(df['datetime'], inplace=True)
+    return df
     
+    
+def reformat_IV(df):
     '''Create dataframe and parse out the current values with correct datetime
      Datetime will be an incrementing millisecond for each value in list 
      at a certain timestamp
@@ -158,19 +157,18 @@ def reformat_IV(trace_id, parameter, string):
             else:
                 i += 1
             output_df = output_df.append({'datetime': pd.to_datetime(row['datetime']) + pd.to_timedelta(f"{i}ms"), parameter: value}, ignore_index=True)
-    f.write("Creating dataframe for current time\n--- %s seconds ---\n" % (time.time() - start2_time))
-
+    f.write("Creating dataframe for current time\n--- %s seconds ---\n\n" % (time.time() - start2_time))
+    f.flush()
     # eliminate Nan values that were created by extra comma at end of each list
     output_df.dropna(0, how='any', inplace=True)
     
     # set index equal to datetime column
     output_df.set_index(output_df['datetime'], inplace=True)
-
+    
     return output_df
 
-
 if __name__ == "__main__":
-    
+
     # Set the variables needed for run_mysql function
     host = ''
     port = ''
@@ -180,37 +178,55 @@ if __name__ == "__main__":
     
     # open logging file
     f = open('interface_logs.txt', 'a+')
-    f.write('\nStart of iteration:\n{0}\n'.format(datetime.datetime.now().time()))
-
+    now = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    f.write('\n=========================================\n')
+    f.write('Start of iteration:\n{0}\n'.format(now))
+    f.write('=========================================\n')
+    f.flush()
     # string 1, current
     trace_id = '8157_UCF_FSEC.UCF_Inverter_1.CB_1.S_1.trace_id'
     string = '8157_S1'
     parameter = 'current'
-    output_df = reformat_IV(trace_id, parameter, string)
+    df = get_mysql_data(trace_id, parameter, string)
+    output_df= reformat_IV(df)
     Store_Vals(output_df, parameter, '8157_UCF_FSEC.UCF_Inverter_1.CB_1.S_1.IV_I')
-
+     
     # string 1, voltage
     string = '8157_S1'
     parameter = 'voltage'
-    output_df = reformat_IV(trace_id, parameter, string)
+    df = get_mysql_data(trace_id, parameter, string)
+    output_df= reformat_IV(df)
     Store_Vals(output_df, parameter, '8157_UCF_FSEC.UCF_Inverter_1.CB_1.S_1.IV_V')   
+    
+    # string 1, trace_id
+    Store_Vals(df, 'trace_id', trace_id)
     
     # string 2, current
     trace_id = '8157_UCF_FSEC.UCF_Inverter_1.CB_1.S_2.trace_id'
     string = '8157_S2'
     parameter = 'current'
-    output_df = reformat_IV(trace_id, parameter, string)
+    df = get_mysql_data(trace_id, parameter, string)
+    output_df= reformat_IV(df)
     Store_Vals(output_df, parameter, '8157_UCF_FSEC.UCF_Inverter_1.CB_1.S_2.IV_I')  
     
     # string 2, voltage
     string = '8157_S2'
     parameter = 'voltage'
-    output_df = reformat_IV(trace_id, parameter, string)
+    df = get_mysql_data(trace_id, parameter, string)
+    output_df= reformat_IV(df)
     Store_Vals(output_df, parameter, '8157_UCF_FSEC.UCF_Inverter_1.CB_1.S_2.IV_V')
-
-
-    f.write('\nEnd of iteration:\n{0}\n'.format(datetime.datetime.now().time()))
-    f.write('sleeping until next prompt...')
     
+    # string 2, trace_id
+    Store_Vals(df, 'trace_id', trace_id)
+    
+    # Calculate number of trace_ids
+    num_trace = df['vals'][-1] - df['vals'][0]
+    now_1 = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    
+    # Calculate total script duration in seconds
+    delta = (pd.to_datetime(now_1) - pd.to_datetime(now)).total_seconds()
+    
+    f.write('Average rate per trace_id: {0}\n'.format((delta / num_trace) / 60))
+    f.write('\nEnd of iteration:\n{0}\n'.format(now_1))
     f.close()
     
