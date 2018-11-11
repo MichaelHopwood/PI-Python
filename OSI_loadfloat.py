75fefc3433e9388a84d4f700df9bd2b7318b56a6
unknown <michael.hopwood@knights.ucf.edu>
2018-11-11 00:36:39 -0500

    Pull_IV function created

--- OSI_loadfloat.py
+++ OSI_loadfloat.py
@@ -6,14 +6,14 @@ Created on Tue Sep 11 11:14:31 2018
 """
 
 import sys
-import clr # Connecting with .NET (PI Database)
+import clr
 
 import pandas as pd
 import numpy as np
 import datetime
 import mysql.connector
 import time
-
+from matplotlib import pyplot
 
 sys.path.append('C:\\Program Files (x86)\\PIPC\\AF\\PublicAssemblies\\4.0')  
 clr.AddReference('OSIsoft.AFSDK')
@@ -28,36 +28,6 @@ from OSIsoft.AF.UnitsOfMeasure import *
 # PI Data Archive
 piServers = PIServers()
 piServer = piServers['net1552.net.ucf.edu']
-
-
-# Still IN BETA
-def get_timestamps(pitag, start, end, freq, timestampcalc = AFTimestampCalculation.MostRecentTime, summarytype = AFSummaryTypes.Maximum):
-    '''Creates dataframe of historical max hourly values for a single PI point'''
-    piServers = PIServers()
-    piServer = piServers['net1552.net.ucf.edu']
-    pt = PIPoint.FindPIPoint(piServer, pitag)
-    timerange = AFTimeRange(start,end)
-    span = AFTimeSpan.Parse(freq)
-    summaries = pt.Summaries(timerange, span, summarytype, AFCalculationBasis.TimeWeighted, timestampcalc)
-    #print("Values updated at: ", datetime.datetime.now())
-    # Loop through and make list
-    times = []
-    vals = []     
-    lastValue = 0
-    for summary in summaries:
-        for event in summary.Value:
-            times.append(str(event.Timestamp.LocalTime))
-            if int(event.Value) > int(lastValue):
-                if get_tag_values(pitag, (pd.to_datetime(str(event.Timestamp.LocalTime)) - pd.Timedelta('+1s')).strftime('%m/%d/%Y %H:%M:%S'), (pd.to_datetime(str(event.Timestamp.LocalTime)) + pd.Timedelta('+1s')).strftime('%m/%d/%Y %H:%M:%S')) is not None:
-                    vals.append(event.Value)
-            event.Value = lastValue
-
-    # Create dataframe
-    df = pd.DataFrame(data = {pitag: vals}, index=times)
-    df.index = pd.to_datetime(df.index)
-
-    return df
-
 
 def Summarize_PI_Data(pitag, start, end, freq, timestampcalc = AFTimestampCalculation.MostRecentTime, summarytype = AFSummaryTypes.Maximum):
     '''Creates dataframe of historical max hourly values for a single PI point'''
@@ -100,46 +70,52 @@ def Summarize_Multi_PIData(pitags, start, end, freq, timestampcalc = AFTimestamp
     return mult_df
 
 def get_tag_values(pitag,timestart,timeend):
+    piServers = PIServers()
+    piServer = piServers['net1552.net.ucf.edu']
     tag = PIPoint.FindPIPoint(piServer, pitag) 
-    timeRange = AFTimeRange(timestart,timeend)
+    timeRange = AFTimeRange(timestart, timeend)
     boundary = AFBoundaryType.Inside
     data = tag.RecordedValues(timeRange,boundary,'',False,0)
     dataList = list(data)
-    #print len(dataList)
-    results = np.zeros((len(dataList), 1), dtype='object') #numpy array
+    results = np.zeros((len(dataList), 2), dtype='object')
     for i, sample in enumerate(data):
-        results[i, :] = float(sample.Value)
+        results[i, 0] = float(sample.Value)
+        results[i, 1] = str(sample.Timestamp.ToString("MM/dd/yyyy HH:mm:ss.fff"))
     
-    df = pd.DataFrame(data=results[0:,0:], columns = [pitag]) 
+    df = pd.DataFrame(data=results[0:,0], index = results[0:,1], columns = [pitag]) 
     return df
 
-# Currently in BETA
-def Pull_IV(IVtags, IVtrace, string, start, end):
-    #Creates a dataframe with historical data for multiple points
+def get_mult_values(pitaglist, timestart, timeend):
     mult_df = pd.DataFrame()
-    df_trace = get_mysql_data(IVtrace, string)
-    df_trace.index = df_trace['datetime']
-    # convert to unix
-#    df_trace['datetime'].astype(np.int64) // 10**9
-    trace_series = pd.to_datetime(df_trace['datetime']).astype(np.int64) // 10**6
-    df_trace_unix = trace_series.to_frame()
-#    start = int(time.mktime(pd.to_datetime(start).timetuple()))
-#    end = int(time.mktime(pd.to_datetime(end).timetuple()))
     
-    print(df_trace['datetime'])
-    print(start)
-    print(end)
-    
-    df_query = df_trace_unix[(df_trace_unix['datetime'] > start) | (df_trace_unix['datetime'] < end)]
-    for IVstart in df_query['datetime']:
-        IVstart = str(IVstart)
-        IVend = (pd.to_datetime(IVstart) + pd.Timedelta(seconds=3)).strftime('%m/%d/%Y %H:%M:%S')
-        for pitag in IVtags:    
-            df = get_tag_values(pitag, IVstart, IVend)
-            mult_df = pd.concat([mult_df, df], axis=1, join = 'outer')
+    for pitag in pitaglist:
+        df = get_tag_values(pitag,timestart,timeend)
+        mult_df = pd.concat([mult_df, df], axis = 1, join = 'outer')
 
-    return mult_df 
+    return mult_df
 
+def get_IV(IVlist, trace_id, timestart, timeend):
+    timeList = []
+    df_traceID = get_tag_values(trace_id, timestart, timeend)
+    trace_list = list(df_traceID.index.values)
+    for IVstart in trace_list:
+        IVstart = pd.to_datetime(IVstart).strftime('%m-%d-%Y %H:%M:%S')
+        IVend = (pd.to_datetime(IVstart) + pd.Timedelta(seconds=4)).strftime('%m-%d-%Y %H:%M:%S')
+        timeList.append([IVstart, IVend])
+        
+    df_list = []
+    for i in range(len(timeList)):
+        df_i = get_tag_values(IVlist[0], timeList[i][0], timeList[i][1])
+        df_v = get_tag_values(IVlist[1], timeList[i][0], timeList[i][1])
+        df = pd.concat([df_i, df_v], axis = 1, join = 'outer', sort = True)
+        df_list.append(df)
+
+    for k in range(len(df_list)):
+        if df_list[k].isnull().values.any() == True:
+            print("There are NaN values in this dataframe. Beware!\n\nInvestigate to see where the NaN values are to resolve the issue.")
+        
+    return df_list
+        
 def rename__cols(df):
     new_colnames = []
     for col in df.columns:
@@ -147,7 +123,6 @@ def rename__cols(df):
     df.columns = new_colnames
     return df
 
-        
 def Store_Vals(df, valuecol, pointname):
     '''
     Store values into the PI System
@@ -199,19 +174,12 @@ def get_mysql_data(trace_id, string):
     return df_trace
     
 if __name__ == "__main__":
-    host = ''
-    port = ''
-    username = ''
-    password = ''
-    database = ''
+    pitaglist = ['8157_UCF.UCF_Inverter_1.CB_1.S_1.IV_I', '8157_UCF.UCF_Inverter_1.CB_1.S_1.IV_V']
+    IV_trace = '8157_UCF.UCF_Inverter_1.CB_1.S_1.trace_id'
+    timestart = '10/30/2018 12:00:00 PM'
+    timeend = '10/30/2018 3:00:00 PM'
+
+    results = get_IV(pitaglist, IV_trace, timestart, timeend)
+    
 
     
-    start = '10/27/2018 12:00:00'
-    end = '10/27/2018 1:00:00'
-    IVtags = ['8157_UCF_FSEC.UCF_Inverter_1.CB_1.S_1.IV_I', '8157_UCF_FSEC.UCF_Inverter_1.CB_1.S_1.IV_V']
-    IVtrace = '8157_UCF_FSEC.UCF_Inverter_1.CB_1.S_2.trace_id'
-    string = '8157_S1'
-    Pull_IV(IVtags, IVtrace, string, start, end)
-
-    
-   
