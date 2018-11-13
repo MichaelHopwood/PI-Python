069df5a831f42b9ab1b33ee9d17321cc644b5911
unknown <michael.hopwood@knights.ucf.edu>
2018-11-13 10:31:19 -0500

    Get_IV function added

--- OSI_loadfloat.py
+++ OSI_loadfloat.py
@@ -13,7 +13,8 @@ import numpy as np
 import datetime
 import mysql.connector
 import time
-from matplotlib import pyplot
+#from matplotlib import pyplot
+import matplotlib.pyplot as plt
 
 sys.path.append('C:\\Program Files (x86)\\PIPC\\AF\\PublicAssemblies\\4.0')  
 clr.AddReference('OSIsoft.AFSDK')
@@ -73,7 +74,7 @@ def get_tag_values(pitag,timestart,timeend):
     piServers = PIServers()
     piServer = piServers['net1552.net.ucf.edu']
     tag = PIPoint.FindPIPoint(piServer, pitag) 
-    timeRange = AFTimeRange(timestart, timeend)
+    timeRange = AFTimeRange.Parse(timestart, timeend)
     boundary = AFBoundaryType.Inside
     data = tag.RecordedValues(timeRange,boundary,'',False,0)
     dataList = list(data)
@@ -94,10 +95,11 @@ def get_mult_values(pitaglist, timestart, timeend):
 
     return mult_df
 
-def get_IV(IVlist, trace_id, timestart, timeend):
+def get_IV(IVlist, trace_id, timestart, timeend, skipNaN = True):
     timeList = []
     df_traceID = get_tag_values(trace_id, timestart, timeend)
     trace_list = list(df_traceID.index.values)
+
     for IVstart in trace_list:
         IVstart = pd.to_datetime(IVstart).strftime('%m-%d-%Y %H:%M:%S')
         IVend = (pd.to_datetime(IVstart) + pd.Timedelta(seconds=4)).strftime('%m-%d-%Y %H:%M:%S')
@@ -110,11 +112,38 @@ def get_IV(IVlist, trace_id, timestart, timeend):
         df = pd.concat([df_i, df_v], axis = 1, join = 'outer', sort = True)
         df_list.append(df)
 
+    badIndexes = []
     for k in range(len(df_list)):
         if df_list[k].isnull().values.any() == True:
-            print("There are NaN values in this dataframe. Beware!\n\nInvestigate to see where the NaN values are to resolve the issue.")
-        
+            print("There are NaN values in this dataframe. Beware!\nInvestigate to see where the NaN values are to resolve the issue.")
+            if skipNaN == True:
+                badIndexes.append(k)
+
+    for x in range(len(df_list)):
+        for y in range(len(badIndexes)):
+            if badIndexes[y] == x:
+                del df_list[x]
+                print("removing element: ", x)
+
     return df_list
+
+def calculate_Rsh(IVcurve):
+    I = '8157_UCF.UCF_Inverter_1.CB_1.S_1.IV_I'
+    V = '8157_UCF.UCF_Inverter_1.CB_1.S_1.IV_V'
+    
+    slope1 = (IVcurve[I][100] - IVcurve[I][0]) / (IVcurve[V][100] - IVcurve[V][0])
+    Rsh1 =  -1 / slope1
+    print("1:", Rsh1)
+    
+    slope2 = (IVcurve[I][75] - IVcurve[I][0]) / (IVcurve[V][75] - IVcurve[V][0])
+    Rsh2 =  -1 / slope2
+    print("2:", Rsh2)
+    
+    slope3 = (IVcurve[I][50] - IVcurve[I][0]) / (IVcurve[V][50] - IVcurve[V][0])
+    Rsh3 =  -1 / slope3
+    print("3:", Rsh3)
+    
+    return
         
 def rename__cols(df):
     new_colnames = []
@@ -172,14 +201,16 @@ def get_mysql_data(trace_id, string):
     # Create dataframe of datetime, groupchannel_name, and trace_id
     df_trace = pd.DataFrame(traceid_list, columns=['datetime', 'group', 'trace_id'])
     return df_trace
+
     
 if __name__ == "__main__":
     pitaglist = ['8157_UCF.UCF_Inverter_1.CB_1.S_1.IV_I', '8157_UCF.UCF_Inverter_1.CB_1.S_1.IV_V']
-    IV_trace = '8157_UCF.UCF_Inverter_1.CB_1.S_1.trace_id'
-    timestart = '10/30/2018 12:00:00 PM'
-    timeend = '10/30/2018 3:00:00 PM'
+    IV_trace = '8157_UCF.UCF_Inverter_1.CB_1.S_1.Isc'
+    timestart = '11/07/2018 1:00:00 PM'
+    timeend = '11/07/2018 4:30:00 PM'
 
-    results = get_IV(pitaglist, IV_trace, timestart, timeend)
+    results = get_IV(pitaglist, IV_trace, timestart, timeend, skipNaN = True)
+
     
-
+        
     
